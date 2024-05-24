bf0bf0341c696f14be9d04b7c617aa2be84c166a
Oviya <oviyasraj2001@gmail.com>
2024-05-24 03:11:40 -0700

    inital commit

--- js/script.js
+++ js/script.js
@@ -3,16 +3,38 @@ function countdown() {
     var countdownElement = document.getElementById('countdown');
     var wordElement = document.getElementById('word');
 
-    function updateCountdown() {
-        countdownElement.textContent = count;
-        count--;
-        if (count >= 0) {
-            setTimeout(updateCountdown, 1000);
-        } else {
-            countdownElement.textContent = "Happy Independence!";
-        }
-    }
+    countdownElement.textContent = count;
 
-    updateCountdown();
+    setTimeout(function() {
+        countdownElement.textContent = --count;
+        setTimeout(function() {
+            countdownElement.textContent = --count;
+            setTimeout(function() {
+                countdownElement.textContent = --count;
+                setTimeout(function() {
+                    countdownElement.textContent = --count;
+                    setTimeout(function() {
+                        countdownElement.textContent = --count;
+                        setTimeout(function() {
+                            countdownElement.textContent = --count;
+                            setTimeout(function() {
+                                countdownElement.textContent = --count;
+                                setTimeout(function() {
+                                    countdownElement.textContent = --count;
+                                    setTimeout(function() {
+                                        countdownElement.textContent = --count;
+                                        setTimeout(function() {
+                                            countdownElement.textContent = --count;
+                                            countdownElement.textContent = "Happy Independence!";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
 }
 countdown();
